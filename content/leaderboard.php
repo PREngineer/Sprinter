27bbd3f890842a065c7e8dc7aac3cdae344aff6a
Jorge Pabon <jorge.l.pabon.cruz@accenturefederal.com>
2018-05-14 17:20:47 -0400

    test

--- content/leaderboard.php
+++ content/leaderboard.php
@@ -9,37 +9,35 @@
 <h1 id="page_title" tabindex="-1" role="heading" aria-level="1">Leaderboard</h1>
 
 <?php
-	echo 'GET: ';
-	print_r($_GET);
-	
-	echo '<br><br>POST: ';
-	print_r($_POST);
-	
 	echo '<br><br>SESSION: ';
 	print_r($_SESSION);
 ?>
+
 <br>
 Leaderboard goes here.
 
 
 <script type="text/javascript">
+	var SprintData = "";
 
-var SprintData = "";
-
-$(document).ready(function ()
-{
-	$('#retrieve-resources').click(function ()
-	{
-		$.ajax(
-		{
-			type: "GET",
-			url: "getSprintData.php",
-			success: function(result)
-			{
-				alert(result);
-			}
-		});
- 
-	});
-});
+    function loadXMLDoc(getSprintData.php)
+    {
+        if (window.XMLHttpRequest)
+        {// code for IE7+, Firefox, Chrome, Opera, Safari, SeaMonkey
+            xmlhttp=new XMLHttpRequest();
+        }
+        else
+        {// code for IE6, IE5
+            xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        xmlhttp.onreadystatechange=function()
+        {
+            if (xmlhttp.readyState==4 && xmlhttp.status==200)
+            {
+                alert(xmlhttp.responseText);
+            }
+        }
+        xmlhttp.open("GET", theURL, false);
+        xmlhttp.send();
+    }
 </script>
