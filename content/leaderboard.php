44d9764c64b020705e8f8e9228fb0e9883910b5f
Jorge Pabon <jorge.l.pabon.cruz@accenturefederal.com>
2018-05-14 18:41:53 -0400

    test

--- content/leaderboard.php
+++ content/leaderboard.php
@@ -11,8 +11,8 @@
 <?php
 
 	$Sprint   = get_SprintData( date("Y-m-d") )[0];
-	$UserData = get_UserData( date("Y-m-d"), $_SESSION['username'] );
-	$UserGoal = get_UserGoal( date("Y-m-d"), $_SESSION['username'] );
+	$UserData = get_UserData( date("Y-m-d"), $_SESSION['userID'] );
+	$UserGoal = get_UserGoal( date("Y-m-d"), $_SESSION['userID'] );
 	
 	print_r($Sprint);
 	echo '<br>';
