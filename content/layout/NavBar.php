0fbafe67e4b0e55dff5b0aa47c3af09fea6beb8e
Jorge Pabon <jorge.l.pabon.cruz@accenturefederal.com>
2018-05-15 11:57:45 -0400

    test

--- content/layout/NavBar.php
+++ content/layout/NavBar.php
@@ -32,7 +32,7 @@
 
         <!-- Brand icon -->
         <a link="index.php" class="navbar-brand" style="cursor: pointer;">
-          <img src="../images/TLogo.png" width="30" height="30" alt="Logo"><span>Sprinter - Health friendly competition</span>
+          <img src="../images/TLogo.png" width="30" height="30" alt="Logo"><span>Sprinter</span>
         </a>
       </div>
 
