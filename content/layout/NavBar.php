c88edf557e33149cd4a2cb0ddc3ffcd8f0f0897e
Jorge Pabon <jorge.l.pabon.cruz@accenturefederal.com>
2018-05-15 12:03:26 -0400

    test

--- content/layout/NavBar.php
+++ content/layout/NavBar.php
@@ -34,7 +34,7 @@
         <a link="index.php" class="navbar-brand" style="cursor: pointer;">
           <img src="../images/TLogo.png" width="30" height="30" alt="Logo">
         </a>
-		<a link="index.php" class="navbar-brand" style="cursor: pointer;">Sprinter - Health friendly competition</a>
+		<a link="index.php" class="navbar-brand" style="cursor: pointer;">Sprinter</a>
       </div>
 
       <!-- Collect the nav links, forms, and other content for toggling -->
