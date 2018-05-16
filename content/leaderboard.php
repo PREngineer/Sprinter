5ff2a1048cfe34f05186c4ed1e628c17e0bd2034
Jorge Pabon <jorge.l.pabon.cruz@accenturefederal.com>
2018-05-16 10:29:07 -0400

    test

--- content/leaderboard.php
+++ content/leaderboard.php
@@ -80,7 +80,9 @@
 	echo '
 			</tbody>
 		</table>
-		
+
+		<br>
+
 		<form action="api.php">
 			<div class="row">
 				<div class=col-lg-6>
@@ -88,14 +90,16 @@
 						<input name="action" type="hidden" value="setGoal">
 						<input name="username" type="hidden" value="' . $_SESSION['userID'] . '">
 						<input name="sprint" type="hidden" value="' . ( ($Sprint)[0] )[0] . '">
-						<input name="goal type="text" class="form-control">
-						<div class="input-group-btn" maxlength="10" placeholder="' . $UserGoal . '" required>
+						<input name="goal type="text" class="form-control" maxlength="10" placeholder="' . $UserGoal . '" required>
+						<div class="input-group-btn">
 							<button type=button class="btn btn-primary">Change Goal</button>
 						</div>
 					</div>
 				</div>
 			</div>
 		</form>
+
+		<br><br>
 		';
 
 
