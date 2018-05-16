006dd2854572d2b046d4bd40c79701e84634752f
Jorge Pabon <jorge.l.pabon.cruz@accenturefederal.com>
2018-05-16 19:24:26 -0400

    test

--- content/login.php
+++ content/login.php
@@ -3,7 +3,7 @@
 include '../functions/Init.php';
 include '../functions/DB.php';
 
-if( isset($_POST['username']) )
+if( sizeof($_POST['username']) > 0 )
 {
 	echo '<script>alert("Inside the action");</script>';
 	$user = $_GET['username'];
